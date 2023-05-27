8b92d85f2ce244d031bc2d46d56603a15cd31b50
TheShoqanebi <admin@theshoqanebi.com>
2023-05-27 09:24:07 +0000

    init

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_m12.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_m12-user \
+    twrp_m12-userdebug \
+    twrp_m12-eng
